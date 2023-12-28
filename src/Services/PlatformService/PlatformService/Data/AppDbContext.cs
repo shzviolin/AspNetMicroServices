8b650a8431f2ab76178b304b7059e9e23041e2cc
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2023-12-28 15:13:46 +0330

    02_Create Platform service

---
+++ src/Services/PlatformService/PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option):base(option)
+        {
+            
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
